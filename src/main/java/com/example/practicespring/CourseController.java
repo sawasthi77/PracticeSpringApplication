e16d9b6d81041e7846b74508f2b33dc17b6148fb
sawasthi77 <saumyaawasthi187@gmail.com>
2023-02-02 11:10:12 +0530

    adding spring spring application files

---
+++ src/main/java/com/example/practicespring/CourseController.java
@@ -0,0 +1,18 @@
+package com.example.practicespring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveCourses(){
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in 28 minutes"),
+                new Course(2, "Learn Devops", "in 28 minutes"));
+    }
+}
